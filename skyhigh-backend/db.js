d3cd9aec4181a65f8407dbddbffcecf37c18670c
Praveena <praveenacce01@gmail.com>
2026-05-18 19:45:55 +0530

    fixed mysql railway connection

--- skyhigh-backend/db.js
+++ skyhigh-backend/db.js
@@ -1,25 +1,27 @@
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_NAME);
-console.log(process.env.DB_PORT);
-
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.log("Database connection failed");
     console.log(err);
   } else {
     console.log("MySQL Connected");
+    connection.release();
   }
 });
 
-module.exports = db;
+module.exports = pool.promise();
