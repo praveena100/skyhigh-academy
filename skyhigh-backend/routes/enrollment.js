d1cdd17f27ced38f1e8d8f041a24525e4f3a1a75
Praveena <praveenacce01@gmail.com>
2026-05-31 22:37:11 +0530

    fix route paths

--- skyhigh-backend/routes/enrollment.js
+++ skyhigh-backend/routes/enrollment.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
-const supabase = require("../../supabase");
+const supabase = require("../supabase");
 
 router.post("/", async (req, res) => {
 
